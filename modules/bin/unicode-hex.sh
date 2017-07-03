05b667860f4bc578981df51d6ad1269cacc69194
Juan Cruz Viotti <jv@jviotti.com>
2017-07-03 12:09:26 -0400

    Add unicode-hex.sh script
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ modules/bin/unicode-hex.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set -e
+set -u
+
+function usage() {
+  echo "Usage: $0 [OPTIONS] <string>"
+  echo ""
+  echo "Options"
+  echo ""
+  echo "    -n normalize string"
+  exit 1
+}
+
+ARGV_NORMALIZE=0
+
+while getopts ":n" option; do
+  case $option in
+    n) ARGV_NORMALIZE=1 ;;
+    *) usage ;;
+  esac
+done
+
+shift $((OPTIND-1))
+set +u
+ARGV_INPUT="$1"
+set -u
+
+if [ -z "$ARGV_INPUT" ]; then
+  usage
+fi
+
+# For when icu4c is installed through brew
+if [ -d "/usr/local/opt/icu4c/bin" ]; then
+  PATH=$PATH:/usr/local/opt/icu4c/bin
+fi
+
+if [ "$ARGV_NORMALIZE" == "1" ]; then
+  UCONV_OPTIONS="-x any-nfd"
+else
+  UCONV_OPTIONS="-x any-nfc"
+fi
+
+echo "$ARGV_INPUT" | uconv $UCONV_OPTIONS | hexdump
