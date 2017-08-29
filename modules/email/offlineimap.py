213e5f5a2f2161cf96761ee15d1677943dbcd709
Juan Cruz Viotti <jv@jviotti.com>
2017-08-29 11:21:15 -0400

    Add get_user offlineimap python function helper
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- modules/email/offlineimap.py
+++ modules/email/offlineimap.py
@@ -4,3 +4,6 @@ from subprocess import check_output
 
 def get_pass(account):
     return check_output("pass key " + account, shell=True).strip("\n")
+
+def get_user(account):
+    return check_output("pass user " + account, shell=True).strip("\n")
