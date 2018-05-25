80d84d0e0c75237c61b3aa2ca804297d914f120c
Juan Cruz Viotti <jv@jviotti.com>
2018-05-25 19:46:52 +0100

    Update retry script
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- modules/bin/retry.sh
+++ modules/bin/retry.sh
@@ -4,27 +4,27 @@ COMMAND="$*"
 TIMES=0
 
 if [ -z "$COMMAND" ]; then
-	echo "Usage: $0 <command...>" 1>&2
-	exit 1
+  echo "Usage: $0 <command...>" 1>&2
+  exit 1
 fi
 
 info () {
-	echo "\nRan $TIMES times"
-	exit 0
+  echo "\nRan $TIMES times"
+  exit 0
 }
 
 trap 'info' SIGINT
 
 while true
 do
-	echo "Running: $COMMAND"
-	$COMMAND
-	EXIT_CODE="$?"
-	((TIMES+=1))
-	if [ "$EXIT_CODE" != "0" ]
-	then
-		echo "Command failed with exit code: $EXIT_CODE" 1>&2
-		echo "After $TIMES retries" 1>&2
-		exit "$EXIT_CODE"
-	fi
+  echo "Running ($TIMES): $COMMAND"
+  $COMMAND
+  EXIT_CODE="$?"
+  ((TIMES+=1))
+  if [ "$EXIT_CODE" != "0" ]
+  then
+    echo "Command failed with exit code: $EXIT_CODE" 1>&2
+    echo "After $TIMES retries" 1>&2
+    exit "$EXIT_CODE"
+  fi
 done
