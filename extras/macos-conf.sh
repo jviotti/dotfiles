6f0fd341fb44ad8dc6886e0c6130cc44f8d3de2a
Juan Cruz Viotti <jv@jviotti.com>
2021-04-13 18:34:38 -0400

    Remove unused stuff
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- extras/macos-conf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Disable Spotlight indexing
-# See http://www.iclarified.com/49187/how-to-disable-and-reenable-spotlight-indexing-on-your-mac
-launchctl unload -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist
