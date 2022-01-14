e2fae74c7da305f2d383005214a41c06b92c604a
Juan Cruz Viotti <jv@jviotti.com>
2022-01-14 19:17:12 -0400

    Detect Windows correctly
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- build/os.mk
+++ build/os.mk
@@ -1,5 +1,19 @@
+ifndef PLATFORM_OS
+ifeq ($(OS),Windows_NT)
+PLATFORM_OS = windows
+ifeq ($(PROCESSOR_ARCHITEW6432),AMD64)
+PLATFORM_ARCH = x86_64
+else
+ifeq ($(PROCESSOR_ARCHITECTURE),AMD64)
+PLATFORM_ARCH = x86_64
+endif
+endif
+endif
+endif
+
+ifndef PLATFORM_ARCH
 PLATFORM_ARCH = $(shell uname -m)
-export PLATFORM_ARCH
+endif
 
 ifndef PLATFORM_OS
 ifeq ($(shell uname),Darwin)
