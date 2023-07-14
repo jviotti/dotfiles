91615da627f45fa2c91b5678d2a59d66787cb073
Juan Cruz Viotti <jv@jviotti.com>
2023-07-14 13:42:35 -0400

    Polish LLDB bridging setup
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- modules/debugger/jviotti.py
+++ modules/debugger/jviotti.py
@@ -1,2 +1,9 @@
+# See https://lldb.llvm.org/python_reference/
+
+# debugger = lldb.SBDebugger
+# result = lldb.SBCommandReturnObject
 def test(debugger, command, result, internal_dict):
-  print("test!")
+  print(debugger.__class__)
+  print(command.__class__)
+  print(result.__class__)
+  print(internal_dict.__class__)
