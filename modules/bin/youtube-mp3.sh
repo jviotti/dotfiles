fc0a4ae22652e4056a8408a04de36bf3d8b67ef5
Juan Cruz Viotti <jv@jviotti.com>
2023-08-28 10:28:04 -0400

    Improve youtube-mp3
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- modules/bin/youtube-mp3.sh
+++ modules/bin/youtube-mp3.sh
@@ -6,4 +6,11 @@ if [ "$#" -ne 1 ]; then
   stdsh_fail "Usage: $0 <url>"
 fi
 
-youtube-dlc --extract-audio --audio-format mp3 "$1"
+if ! stdsh_has_command "yt-dlp"
+then
+  echo "Download yt-dlp from https://github.com/yt-dlp/yt-dlp" 1>&2
+  exit 1
+fi
+
+yt-dlp --update
+yt-dlp --extract-audio --audio-format mp3 "$1"
