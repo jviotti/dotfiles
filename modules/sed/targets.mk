a687b1c3324f7f75d926f7486f05197ab1502296
Juan Cruz Viotti <jv@jviotti.com>
2024-02-07 10:11:59 -0400

    Add "replace" helper script
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- modules/sed/targets.mk
+++ modules/sed/targets.mk
@@ -1,4 +1,5 @@
 $(DESTINATION)/bin/%: modules/sed/% | $(DESTINATION)/bin
 	$(SYMLINK)
-sed: $(DESTINATION)/bin/gres $(DESTINATION)/bin/runsed.sh $(DESTINATION)/bin/testsed.sh
+sed: $(DESTINATION)/bin/gres $(DESTINATION)/bin/replace \
+	$(DESTINATION)/bin/runsed.sh $(DESTINATION)/bin/testsed.sh
 MODULES += sed
