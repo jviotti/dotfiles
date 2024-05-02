33d8649d28fca009a21325ac85ec61e78f196deb
Juan Cruz Viotti <jv@jviotti.com>
2024-05-02 11:04:26 -0400

    Improve `email` script to work better on WSL
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- modules/email/email.sh
+++ modules/email/email.sh
@@ -3,6 +3,10 @@
 set -o errexit
 set -o nounset
 
+# Go to a safe location before fetching e-mail. On i.e. WSL, `mbsync`
+# will fail if it runs on a Windows directory vs a WSL directory.
+cd "$HOME"
+
 # mbsync often fails with EOF from Mailfence's IMAP. Retrying enough times seem
 # to make it work, so it might be a race condition that can be circunvented by
 # just retrying various times until it works.
