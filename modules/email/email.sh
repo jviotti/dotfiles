0bbeb58374c5e02ecc31beb80459e2c6bdb5934e
Juan Cruz Viotti <jv@jviotti.com>
2024-05-09 12:12:06 -0400

    Simplify `email` alias
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- modules/email/email.sh
+++ modules/email/email.sh
@@ -7,15 +7,4 @@ set -o nounset
 # will fail if it runs on a Windows directory vs a WSL directory.
 cd "$HOME"
 
-# mbsync often fails with EOF from Mailfence's IMAP. Retrying enough times seem
-# to make it work, so it might be a race condition that can be circunvented by
-# just retrying various times until it works.
-
-RETRIES=10
-EXIT_CODE=1
-while [ "$RETRIES" -gt 0 ] && [ "$EXIT_CODE" != "0" ]
-do
-  mbsync --all && EXIT_CODE="$?" || EXIT_CODE="$?"
-  RETRIES="$((RETRIES-1))"
-done
-exit "$EXIT_CODE"
+exec mbsync --all
