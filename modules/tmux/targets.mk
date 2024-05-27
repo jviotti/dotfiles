768ed5873dcc8c7b2743f94a1316e58ed98b83bc
Juan Cruz Viotti <jv@jviotti.com>
2024-05-27 18:49:58 +0100

    Remove batter indicator in tmux
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- modules/tmux/targets.mk
+++ modules/tmux/targets.mk
@@ -1,4 +1,3 @@
-$(DESTINATION)/bin/battery: modules/tmux/battery; $(SYMLINK)
 $(DESTINATION)/.tmux.conf: modules/tmux/tmux.conf.$(PLATFORM); $(SYMLINK)
-tmux: $(DESTINATION)/bin/battery $(DESTINATION)/.tmux.conf
+tmux: $(DESTINATION)/.tmux.conf
 MODULES += tmux
