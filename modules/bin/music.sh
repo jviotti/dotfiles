4777bcd8fca5bc05fa1d0b2348b1c921e0770f2d
Juan Cruz Viotti <jv@jviotti.com>
2025-01-03 08:59:38 -0400

    Argument checking for `music.sh`
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- modules/bin/music.sh
+++ modules/bin/music.sh
@@ -2,6 +2,12 @@
 
 # From https://gssparks.github.io/post/a-simple-command-line-youtube-music-player/
 
+if [ "$#" -lt 1 ]
+then
+  echo "Usage: $0 <query>" 1>&2
+  exit 1
+fi
+
 SEARCH=$*
 echo "Playing Music based on the search: $SEARCH" 1>&2
 mpv --shuffle --no-video ytdl://ytsearch10:"$SEARCH" --ytdl-format=bestaudio
